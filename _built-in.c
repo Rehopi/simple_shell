9849d25271b7f627e70a9d7263fb9eeee69a1392
Rehopi <104981996+Rehopi@users.noreply.github.com>
2022-08-09 00:34:23 +0100

    'updated'

---
+++ _built-in.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+#include <string.h>
+/**
+ * _builtin_ - find the command inside path
+ * @path: double pointer to char array input
+ * @cmd: pointer to char array input
+ * Return: 1 if function is builtin or 0 if function is no builtin
+ */
+int _builtin_(char **path, char *cmd)
+{
+	int i = 0;
+
+	for (i = 0; path[i]; i++)
+	{
+		if (strcmp(cmd, path[i])== 0)
+			return (1);
+	}
+	return (0);
+}
+
+
+	
